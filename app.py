46476cfb420e5c4ee30064567296bd35a6d41b64
Eileen Chau <echau@amazon.com>
2026-02-11 17:12:40 -0800

    feat: Add Cosmos DB appointments endpoint to Flask app

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+import os
 from flask import Flask
+from azure.cosmos import CosmosClient
 
 app = Flask(__name__)
 
@@ -9,6 +11,20 @@ def hello():
 @app.route('/')
 def health():
     return {'status': 'healthy'}
+
+@app.route('/api/appointments')
+def get_appointments():
+    endpoint = os.environ.get('COSMOS_DB_ENDPOINT', 'https://apmwebapp-server.documents.azure.com:443/')
+    key = os.environ.get('COSMOS_DB_KEY')
+    if not key:
+        return {'error': 'COSMOS_DB_KEY not configured'}, 500
+    
+    client = CosmosClient(endpoint, key)
+    database = client.get_database_client(os.environ.get('COSMOS_DB_DATABASE', 'petclinic'))
+    container = database.get_container_client(os.environ.get('COSMOS_DB_CONTAINER', 'appointments'))
+    
+    items = list(container.query_items('SELECT * FROM c', enable_cross_partition_query=True, max_item_count=10))
+    return {'appointments': items}
 
 if __name__ == '__main__':
     app.run()
